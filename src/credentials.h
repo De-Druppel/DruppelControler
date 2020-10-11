4501e05778634160a11544743367f3eb1074d930
Derek <derekmeuwissen@gmail.com>
2020-10-11 22:57:09 +0200

    Implemented Wifi + MQTT

---
+++ src/credentials.h
@@ -0,0 +1,8 @@
+#define SSID "GNX08F865_5G"
+#define WIFI_PASSWORD "BE60FAFD22
+#define MQTT_HOST "ec2-100-27-12-57.compute-1.amazonaws.com"
+#define MQTT_PORT 1883
+#define MQTT_USER "client"
+#define MQTT_PASSWORD "Cn*8uw9kmnMUjqSXh!BAe&NdwDVmQpWTDTV$V&%aWgjJa5MPFZg9rzvwPb8HPH8X"
+
+
---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <credentials.h>
+#include <ESP8266WiFi.h>
+
+WiFiClient wifiClient;
+
+PubSubClient pubsubClient;
+
+String ESP_ID = String(ESP.getChipId());
+
+void setup() { 
+  pubsubClient = configureMQTTClient(pubsubClient,wifiClient,MQTT_HOST,MQTT_PORT);  
+}
+
+void loop() {
+  if(!isWifiConnected())
+  {
+    connectWifi(SSID,WIFI_PASSWORD);
+  }  
+
+  if(!isMQTTConnected())
+  {
+    connectMQTT(ESP_ID.c_str(), MQTT_USER,MQTT_PASSWORD);
+  }
+
+
+
+  delay(100);
+}
+
+void connectWifi(char* ssid, char* wifiPassword)
+{
+  WiFi.begin(ssid,wifiPassword);
+}
+
+bool isWifiConnected()
+{
+  return WiFi.status() == WL_CONNECTED;
+}
+
+PubSubClient configureMQTTClient(PubSubClient pubSubClient,WiFiClient wifiClient, char* mqttHost, uint16_t mqttPort)
+{
+  PubSubClient pubsubClient(wifiClient);
+  pubsubClient.setServer(mqttHost, mqttPort);  
+  return pubsubClient;
+}
+
+void connectMQTT(const char* mqttId, char* mqttUser, char* mqttPassword)
+{
+  pubsubClient.connect(mqttId,mqttUser,mqttPassword);
+}
+
+bool isMQTTConnected()
+{
+  return pubsubClient.connected();
+}
+
