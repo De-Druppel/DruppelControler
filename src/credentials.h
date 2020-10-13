b8598b67aaee8e7675a34bffac0ca15f7f65bc34
pgaskey <peter_g_askey@live.co.uk>
2020-10-13 08:51:59 +0200

    Delete credentials.h

--- src/credentials.h
+++
@@ -1,7 +0,0 @@
-#define SSID "Paskey_wifi_5G"
-#define WIFI_PASSWORD "HailToH00ts"
-#define MQTT_USER "client"
-#define MQTT_PASSWORD "Cn*8uw9kmnMUjqSXh!BAe&NdwDVmQpWTDTV$V&%aWgjJa5MPFZg9rzvwPb8HPH8X"
-#define MQTT_HOST "ec2-100-27-12-57.compute-1.amazonaws.com"
-#define MQTT_PORT 1883
-#define SOIL_MOISTURE_THRESHOLD 750
